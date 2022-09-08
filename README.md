89df675558807124c09856685d9e3b9c0997d088
Lucas Sansberro <lucas_sansberro@outlook.com>
2022-09-08 17:50:54 -0300

    Individual Meal Item added

---
+++ README.md
@@ -0,0 +1,4 @@
+### Food order app
+
+A little practice page made with React.js for the eleventh module of the
+[Udemy React course by Maximilian Schwarzmüller](https://www.udemy.com/course/react-the-complete-guide-incl-redux/)
---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Header from "./Layout/Header";
+import Meals from "./Meals/Meals"
+
+function App() {
+  return (
+    <>
+      <Header />
+      <main>
+        <Meals />
+      </main>
+    </>
+  );
+}
+
+export default App;
