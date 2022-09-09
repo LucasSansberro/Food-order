a2e6ea626ea06af2780802800b3ec20926acb6a0
Lucas Sansberro <lucas_sansberro@outlook.com>
2022-09-09 19:02:46 -0300

    Context, ref and reducer added

--- src/Meals/MealItem/MealItem.jsx
+++ src/Meals/MealItem/MealItem.jsx
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useContext } from "react";
 import classes from "./MealItem.module.css";
+import MealItemForm from "./MealItemForm";
+import CartContext from "../../store/cart-context";
 
 const MealItem = (props) => {
+  const cartCtx = useContext(CartContext);
+  const addToCartHandler = (amount) => {
+    cartCtx.addItem({
+      id: props.id,
+      name: props.name,
+      amount: amount,
+      price: props.price,
+    });
+  };
+
   return (
     <li className={classes.meal}>
       <div>
@@ -9,7 +21,9 @@ const MealItem = (props) => {
         <div className={classes.description}>{props.description}</div>
         <div className={classes.price}>${props.price.toFixed(2)}</div>
       </div>
-      <div></div>
+      <div>
+        <MealItemForm id={props.id} onAddToCart={addToCartHandler} />
+      </div>
     </li>
   );
 };
--- src/Meals/MealItem/MealItemForm.jsx
+++ src/Meals/MealItem/MealItemForm.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import React, { useRef, useState } from "react";
+import classes from "./MealItemForm.module.css";
+import Input from "../../UI/Input";
 
-const MealItemForm = () => {
+const MealItemForm = (props) => {
+  const [amountIsValid, setAmountIsValid] = useState(true);
+  const amountInputRef = useRef();
+
+  const submitHandler = (event) => {
+    event.prevent.default();
+    const enteredAmount = amountInputRef.current.value;
+    const enteredAmountNumber = +enteredAmount;
+    if (
+      enteredAmount.trim().length === 0 ||
+      enteredAmountNumber < 1 ||
+      enteredAmountNumber > 5
+    ) {
+      setAmountIsValid(false);
+      return;
+    }
+
+    props.onAddToCart(enteredAmountNumber);
+  };
+
   return (
-    <div>MealItemForm</div>
-  )
-}
+    <form className={classes.form} onSubmit={submitHandler}>
+      <Input
+        ref={amountInputRef}
+        label="Amount"
+        input={{
+          id: "amount_" + props.id,
+          type: "number",
+          min: "1",
+          max: "5",
+          step: "1",
+          defaultValue: "1",
+        }}
+      />
+      <button>+ Add</button>
+      {!amountIsValid && <p>Please ented a valid amount (1-5).</p>}
+    </form>
+  );
+};
 
-export default MealItemForm
+export default MealItemForm;
